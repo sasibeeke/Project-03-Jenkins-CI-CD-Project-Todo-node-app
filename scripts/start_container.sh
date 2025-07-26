b1683e9737ce90445ec7e45dae22acabbd1f417d
sasibeeke <42212652+sasibeeke@users.noreply.github.com>
2025-07-26 11:50:01 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # pulling docker images
-docker pull amitabhdevops/aws-node-todo-app-cicd:v1
+docker pull sasibk/aws-node-todo-app-cicd:v1
 
 # Run the Docker image as a container
-docker run -d -p 8000:8000 amitabhdevops/aws-node-todo-app-cicd:v1
+docker run -d -p 8000:8000 sasibk/aws-node-todo-app-cicd:v1
